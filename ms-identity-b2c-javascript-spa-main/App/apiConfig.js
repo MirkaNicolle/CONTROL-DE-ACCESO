7fe23bcc37550fbd672f2a4371750b43955dbf69
Mirka <mirkanicolle@gmail.com>
2023-02-20 18:35:21 -0600

    fronted

---
+++ ms-identity-b2c-javascript-spa-main/App/apiConfig.js
@@ -0,0 +1,5 @@
+// The current application coordinates were pre-registered in a B2C tenant.
+const apiConfig = {
+    b2cScopes: ["https://CONTOSOUVG.onmicrosoft.com/b06972b6-d8e4-4180-8d64-421b43afff78/demo.readnp"],
+    webApi: "http://localhost:5000"
+  };
